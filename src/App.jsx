c11d896ca1f4a573a8b342b5808c03b19d54f135
Frogtreee <meteotime9@gmail.com>
2025-10-16 22:58:00 -0400

    added preview

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,23 @@ import './App.css'
 function App() {
 
   //projects holder
-  // let project_1 = {title: '', imageSource: '.../src/images/'}
-  let project_1 = {title: 'DUMP', imageSource: "/src/images/ChatGPT Image Sep 22, 2025, 01_24_39 AM.png"}
-  let project_2 = {title: 'deadSpace', imageSource: '/src/images/deadspace.avif'}
+  // let project_1 = {title: '', imageSource: '.../src/images/', role: '', duration: '', details: ''}
+  let project_1 = {title: 'DUMP', imageSource: "/src/images/ChatGPT Image Sep 22, 2025, 01_24_39 AM.png", role: 'developer', duration: '10/28/2025', details: 'dsjhfaksdjfhaskjdfhaskldjfhsakjldfhskajdlfhaskljdfhsadkjlfhsdakjlfhaskdjlfhsakjldfhkajlsdhfkjlsadhfkljsdafhkjlsadfhkjlsadfhklsjadfhsjkadlfhskajldfhsajkldfhsakldjhfsklajdfh'}
+  let project_2 = {title: 'deadSpace', imageSource: '/src/images/deadspace.avif', role: 'developer', duration: '10/28/2025', details: 'dsjhfaksdjfhaskjdfhaskldjfhsakjldfhskajdlfhaskljdfhsadkjlfhsdakjlfhaskdjlfhsakjldfhkajlsdhfkjlsadhfkljsdafhkjlsadfhkjlsadfhklsjadfhsjkadlfhskajldfhsajkldfhsakldjhfsklajdfh'}
 
   let [isOpen, setIsOpen] = useState(false)
   let [isClicked, setClick] = useState(0)
   let [projectsPackage, setPacks1] = useState([project_2, project_2, project_2, project_1, project_1])
   let [imagesPackage, setPacks2] = useState([project_1, project_1, project_1, project_1, project_1, project_1, project_1])
   //change to key value pair later on
+
+  let [isPreviewOpen, setisPreviewOpen] = useState(false);
+  let [selectedArr, setSelectedArr] = useState(0);
+
+  function openPreview(i){
+    setisPreviewOpen(true);
+    setSelectedArr(i);
+  }
 
   return (
     <>
@@ -60,9 +68,12 @@ function App() {
           </div>
           <div className='displayer'>
             {
-              isClicked == 0 ? <Boxholder packages = { projectsPackage }/> : null ||
+              isClicked == 0 ? <Boxholder packages = { projectsPackage } openPreview = {openPreview} selectedArr = { selectedArr } isPreviewOpen = { isPreviewOpen } setisPreviewOpen = { setisPreviewOpen }/> : null ||
               isClicked == 1 ? <Boxholder packages = { imagesPackage }/> : null ||
               isClicked == 2 ? <InfoModal></InfoModal> : null
+            }
+            {
+              isPreviewOpen == true ? <></> : null
             }
           </div>
         </div>
@@ -77,20 +88,53 @@ function Boxholder(props){
       {
       props.packages.map(function(arrIndex, i){
         return(
-          <Box arrIndex = {arrIndex} key = { i }/>
+          <img src={arrIndex.imageSource} className='box' key = { i } onClick={() => { props.openPreview(i) }}>
+          </img>
         )
       })
+      }
+      {
+        props.isPreviewOpen && (<Preview packages = { props.packages } setisPreviewOpen = { props.setisPreviewOpen } selectedArr = { props.selectedArr }></Preview>)
       }
     </div>
   )
 }
 
-function Box(props){
-  return (
-    <img src={props.arrIndex.imageSource} className='box'>
-    </img>
+function Preview(props){
+  return(
+  <div className="overlay" onClick={() => {props.setisPreviewOpen(false)}}>
+    <div className="popup" onClick={(e) => e.stopPropagation()}>
+      <div className='preview_Header'>
+        <h1>{ props.packages[props.selectedArr].title }</h1>
+        <button onClick={() => {props.setisPreviewOpen(false)}}>Close</button>
+      </div>
+      <div className='preview_imageAnddetails'>
+        <img src = { props.packages[props.selectedArr].imageSource }>
+        </img>
+        <div className='preview_details'>
+          <button>
+            More
+          </button>
+          <h4>
+            duration: <br></br>
+            {props.packages[props.selectedArr].duration}
+          </h4>
+          <h4>
+            role: {props.packages[props.selectedArr].role}
+          </h4>
+        </div>
+      </div>
+      <h4>
+        About
+      </h4>
+      <p>
+        {props.packages[props.selectedArr].details}
+      </p>
+    </div>
+  </div>
   )
 }
+
 
 function InfoModal(){
   return(
